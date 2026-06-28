54e397bccc0890c048cb59c6fda56db9e3488ced
Nikita Sergeevich <yanenekit@gmail.com>
2026-06-28 16:32:15 +0300

    Step 12: init_zero_bias

--- model.py
+++ model.py
@@ -83,8 +83,11 @@ def he_init(shape, fan_in, seed):
     weights=np.random.normal(loc=0.0, scale=std, size=shape)
     return weights
 
-# Step 12 - init_zero_bias (not yet solved)
-# TODO: implement
+# Step 12 - init_zero_bias
+import numpy as np
+
+def init_zero_bias(length):
+    return np.zeros((length,)).T
 
 # Step 13 - pad_2d (not yet solved)
 # TODO: implement

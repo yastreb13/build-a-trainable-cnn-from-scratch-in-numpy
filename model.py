2e5016a62f1c7f193372815d4aab7c8e9a9bc0c2
Nikita Sergeevich <yanenekit@gmail.com>
2026-07-07 18:22:36 +0300

    Step 42: init_conv_layer

--- model.py
+++ model.py
@@ -399,8 +399,16 @@ def adam_step(param, grad, m, v, t, lr, beta_one, beta_two, eps):
     param=adam_param_step(param, m_hat, v_hat, lr, eps)
     return param, m, v
 
-# Step 42 - init_conv_layer (not yet solved)
-# TODO: implement
+# Step 42 - init_conv_layer
+def init_conv_layer(out_channels, in_channels, kernel_size, seed=0):
+    b=init_zero_bias(out_channels)
+    fan_in = in_channels * kernel_size * kernel_size
+    W=he_init((out_channels, in_channels, kernel_size, kernel_size),fan_in,seed=seed)
+    dicti={
+        'W':W,
+        'b':b
+    }
+    return dicti
 
 # Step 43 - init_linear_layer (not yet solved)
 # TODO: implement
